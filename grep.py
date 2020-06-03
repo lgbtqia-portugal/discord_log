2150bf7c1fcb1b7cc0482c25809420a2403032b9
raylu <lurayl@gmail.com>
2020-06-02 17:13:32 -0700

    grep: case-insensitive, show author_id

--- grep.py
+++ grep.py
@@ -7,7 +7,7 @@ import sys
 import lz4framed
 
 def main():
-	query = sys.argv[1]
+	query = sys.argv[1].casefold()
 	for file_path in sys.argv[2:]:
 		search_path(query, file_path)
 
@@ -16,8 +16,8 @@ def search_path(query, file_path):
 		for filename in os.listdir(file_path):
 			search_path(query, path.join(file_path, filename))
 	else:
-		for time, contents in search_file(query, file_path):
-			print(file_path, time, contents)
+		for author_id, time, contents in search_file(query, file_path):
+			print(file_path, author_id, time, contents)
 
 def search_file(query, full_path):
 	with open(full_path, 'rb') as f:
@@ -28,8 +28,8 @@ def search_file(query, full_path):
 	for message in contents.split(b'\0')[:-1]:
 		msg_id, time, author_id, contents = message.split(b'|', 3)
 		contents = contents.decode('utf-8')
-		if query in contents:
-			yield time.decode('ascii'), contents
+		if query in contents.casefold():
+			yield author_id.decode('ascii'), time.decode('ascii'), contents
 
 if __name__ == '__main__':
 	main()
